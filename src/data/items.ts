b721b8cec11d0ec46d5791d24dd110c307c12a64
Rander Carrijo <rander.carrijo@gmail.com>
2022-11-18 22:57:48 -0300

    adding files to repo

---
+++ src/data/items.ts
@@ -0,0 +1,16 @@
+import android from '../svgs/android.svg';
+import caminhao from '../svgs/caminhao.svg';
+import disney from '../svgs/disney.svg';
+import estrela from '../svgs/estrela.svg';
+import gasolina from '../svgs/gasolina.svg';
+import moto from '../svgs/moto.svg';
+
+export const items = [
+    {name: 'android', Icon:android},
+    {name: 'caminhao', Icon:caminhao},
+    {name: 'disney', Icon:disney},
+    {name: 'estrela', Icon:estrela},
+    {name: 'gasolina', Icon:gasolina},
+    {name: 'moto', Icon:moto}
+
+];
